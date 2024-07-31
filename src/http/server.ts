6ada51ee8aa8cf56ed08085bbc8ce5f44dfce098
João Silva <jpsouzadasilva@gmail.com>
2024-07-31 04:04:35 -0300

    feature: add validation and default error

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,6 +13,19 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurant)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+        return error.toResponse
+      }
+      default: {
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
 
 app.listen(8080, () => {
   console.log('Server running on port 8080');
