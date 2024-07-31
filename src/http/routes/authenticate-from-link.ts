7647386ffce43b61a5fa5544d1f960b29bb98f18
João Silva <jpsouzadasilva@gmail.com>
2024-07-31 03:09:25 -0300

    feature: add get profile route

--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -39,7 +39,7 @@ export const authenticateFromLink = new Elysia()
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
 
-    return redirect(redirectUrl)
+    // return redirect(redirectUrl)
 
   }, {
     query: t.Object({
